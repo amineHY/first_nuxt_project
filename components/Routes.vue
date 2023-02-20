864ef28a64122fe92eb993b71bd66a35a31259d8
Amine <hadjyoucef.amine@gmail.com>
2023-02-20 12:53:50 +0100

    minor fix

--- components/Routes.vue
+++ components/Routes.vue
@@ -1,10 +1,16 @@
 <template>
-    <div>
+    <div class="flex justify-center">
         <h2>Available Routes:</h2>
-        <ul>
+        <ul class=" text-left mx-auto">
             <li v-for="route in $router.options.routes" :key="route.path">
                 <router-link :to="route.path">{{ route.path }} - {{ route.name }}</router-link>
             </li>
         </ul>
     </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
