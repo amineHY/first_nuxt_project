3fbbb93b842710b3ce801cb7d4fadfede14201a3
Amine HY <hadjyoucef.amine@gmail.com>
2023-08-27 11:06:49 +0200

    add files

---
+++ layouts/blog.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Header />
+        Some *custom* layout
+
+        <div class="px-10 pt-10 pb-40 md:pt-44 lg:pb-56 lg:pt-36 text-center z-20">
+            <slot />
+        </div>
+
+        <Footer />
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+    text-align: left;
+}
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="sect1">
+        <h2 id="_articles_sur_le_machine_learning" class="tex">
+            Articles sur le Machine Learning
+        </h2>
+        <div class="sectionbody">
+            <div class="ulist">
+                <ul>
+                    <li>
+                        <p>
+                            <a href="blog/guide_ml.html" target="_blank" rel="noopener">Guide de Data Science et Machine
+                                Learning</a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="sect1">
+        <h2 id="_articles_publiés_dans_medium">Articles publiés dans Medium</h2>
+        <div class="sectionbody">
+            <div class="ulist">
+                <ul>
+                    <li>
+                        <p>
+                            <a href="https://amine-hy.medium.com/data-visualization-using-redash-on-the-cloud-63f6d4f2f1ef"
+                                target="_blank" rel="noopener">Data Visualization using Redash on the Cloud</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a href="https://medium.com/towards-data-science/convolutional-neural-network-for-image-classification-with-implementation-on-python-using-pytorch-7b88342c9ca9"
+                                target="_blank" rel="noopener">Deep Learning for Image Classification with
+                                Implementation in
+                                PyTorch</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a href="https://medium.com/towards-data-science/have-you-optimized-your-deep-learning-model-before-deployment-cdc3aa7f413d"
+                                target="_blank" rel="noopener">Have you Optimized your Deep Learning Model Before
+                                Deployment?</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a href="https://medium.com/towards-data-science/how-to-package-a-python-application-using-anaconda-and-docker-fc752ce47729"
+                                target="_blank" rel="noopener">How to Package A Python Application using Anaconda and
+                                Docker</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a href="https://medium.com/towards-data-science/how-to-write-and-publish-a-research-paper-3692550a5c5d"
+                                target="_blank" rel="noopener">How to Write and Publish a Research Paper</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a href="https://amine-hy.medium.com/model-centric-vs-data-centric-view-in-the-age-of-ai-b59c15a53fc4?source=your_stories_page-------------------------------------"
+                                target="_blank" rel="noopener">Model-centricvs Data-centric View in the age of AI</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a href="https://medium.com/swlh/pensar-sdk-1-647f778bc11" target="_blank" rel="noopener">Pensar
+                                SDK: A Rapid ArtificialIntelligence Application
+                                Development Framework on the Edge Device</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a href="https://towardsdatascience.com/simplify-your-data-science-project-with-this-tool-c493b9970280"
+                                target="_blank" rel="noopener">Simplify your Data Science Project with this Tool</a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: "blog",
+});
+</script>
+
+<style lang="scss" scoped></style>
